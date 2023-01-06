1934235639e26ad449afe6461c1f0d7b2f9718b6
irfanirshad <84003624+irfanirshad@users.noreply.github.com>
2023-01-06 08:03:45 +0530

    initial commit

---
+++ theme/src/config/nav.js
@@ -0,0 +1,26 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'Blog',
+		slug: '/blog',
+	},
+	{
+		title: 'About',
+		slug: '/company/about',
+	},
+	{
+		title: 'Our Services',
+		slug: '/company/about1',
+	},
+	{
+		title: 'Contact',
+		slug: '/company/contact',
+	},
+	{
+		title: 'Legal',
+		slug: '/company/legal',
+	},
+];
---
+++ theme/src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `SN Enterprises | Chennai Real Estate Services`,
+	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
+	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	name: `SNE`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: true,
+	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
