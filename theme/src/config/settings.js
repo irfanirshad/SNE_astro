69d82922c4ab62582fbb5454bb7c46e51c42ed50
irfanirshad <84003624+irfanirshad@users.noreply.github.com>
2023-01-09 07:45:38 +0530

    added google analytics

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
-	title: `SNE | Chennai Office Space for Rent`,
+	title: `Chennai Office Spaces for Rent Instant`,
 	description: `Chennai Office Space for Rent. Book your office in Chennai by tomorrow`,
-	url: `https://snechennai.com`, // No trailing slash!
+	url: `https://www.snechennai.com`, // No trailing slash!
 	name: `SNE`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
