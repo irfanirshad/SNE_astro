124a87e88c7e2499973756c08e4098b93122a5b5
Irfan <aaaii@Irfans-MacBook-Pro.local>
2023-03-05 05:08:03 +0530

    seo params changed

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Office Spaces for Rent in Chennai. Fully furnished office space for rent in Chennai.Book your office in Chennai by tomorrow`,
-	description: `Office Spaces for Rent in Chennai. Fully furnished office space for rent in Chennai. Book your office in Chennai by tomorrow`,
+	title: `Office Spaces for Rent in Chennai. Fully furnished office space for rent in Chennai.Book your office in Chennai by tomorrow | SNE Realty`,
+	description: `Office Spaces for Rent in Chennai. Fully furnished office space for rent in Chennai. Book your office in Chennai by tomorrow | SNE Realty`,
 	url: `https://www.snechennai.com`, // No trailing slash!
-	name: `SNE`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `SNE REALTY`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
