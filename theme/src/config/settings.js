dbb7810ec08ec0b3a4c8cad918a9b048ef753905
Irfan <aaaii@Irfans-MacBook-Pro.local>
2023-03-20 02:17:22 +0530

    home page changes

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -2,7 +2,7 @@ export default {
 	title: `Office Spaces for Rent in Chennai. Fully furnished office space for rent in Chennai.Book your office in Chennai by tomorrow | SNE Realty`,
 	description: `Office Spaces for Rent in Chennai. Fully furnished office space for rent in Chennai. Book your office in Chennai by tomorrow | SNE Realty`,
 	url: `https://www.snechennai.com`, // No trailing slash!
-	name: `SNE REALTY`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `SNE REALTY  ,    Tel:+919841140122`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
