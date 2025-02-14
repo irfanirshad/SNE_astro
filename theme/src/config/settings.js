371bda3bb0dcb582addb24864ccf863de09e3dfa
irfanirshad <irfanirshad003@gmail.com>
2025-02-15 03:32:41 +0500

    Changed routes

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Office Spaces for Rent in Chennai. Fully furnished office space for rent in Chennai.Book your office in Chennai by tomorrow | SNE Realty`,
 	description: `Office Spaces for Rent in Chennai. Fully furnished office space for rent in Chennai. Book your office in Chennai by tomorrow | SNE Realty`,
-	url: `https://www.snechennai.com`, // No trailing slash!
+	url: `https://www.snechennai.in`, // No trailing slash!
 	name: `SNE REALTY  | Tel:+919841140122  | snenterpriseschennai@gmail.com`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
